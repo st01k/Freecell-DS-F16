c8c14db56f4a2abdb834d7cccf2129bf04d706f0
Ryan <rwhytsell67@gmail.com>
2016-10-26 08:56:11 -0400

    TeSt

---
+++ src/graphicalUI/GUI.java
@@ -0,0 +1,12 @@
+package graphicalUI;
+
+import freecell.GameBoardInterface;
+
+public interface GUI 
+{
+	public void paint(GameBoardInterface gb);
+	
+	public void initialize(GameBoardInterface gb);
+	
+	//El Test
+}
