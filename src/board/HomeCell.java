94278481be12b0e5e53bcd9cebc82925f469ef98
Casey Murphy <st01k@lvaas.net>
2016-11-02 20:36:03 -0400

    test from bDev merge

--- src/board/HomeCell.java
+++ src/board/HomeCell.java
@@ -52,7 +52,7 @@ public class HomeCell implements CellInterface {
 	 */
 	public StdCard peekCard() {
 		return cell;
-	}
+	}// 
 	
 	// cell checks ------------------------------------------------------------
 	/**
